5bf39b1f9dbc204d3c3f32531ae886b63d627a59
yiphoipan <yiphoipan@yiphoipandeMacBook-Pro.local>
2019-06-11 18:49:59 +0800

    搜索中心删除功能

--- public/front/js/search.js
+++ public/front/js/search.js
@@ -16,8 +16,50 @@ $(function () {
     }
 
     $('.lt_history').on('click','.btn-empty',function () {
-        localStorage.removeItem('search_list');
+        mui.confirm('你确定要清空历史记录吗','温馨提示',['取消','确认'],function (e) {
+            if (e.index === 1){
+                localStorage.removeItem('search_list');
+                render();
+            }
+        })
+
+    })
+
+    $('.lt_history').on('click','.btn_del',function () {
+        var that=this;
+        mui.confirm('你确定要删除该条记录吗','温馨提示',['取消','确认'],function (e) {
+            if (e.index===1){
+                var index= $(this).data('data-index');
+                var arr=getHistory();
+                arr.splice(index, 1,);
+                localStorage.setItem('search_list',JSON.stringify(arr));
+                render();
+            }
+        })
+
+
+    })
+
+
+    $('.search_btn').click(function () {
+        var key = $('.search_input').val().trim();
+        if (key === ''){
+            alert('请输入搜索关键字');
+            return
+        }
+        var arr=getHistory();
+        var index = arr.indexOf(key);
+        if (index!=-1){
+            arr.splice(index, 1);
+        }
+        if (arr.length>=10){
+            arr.pop();
+        }
+        arr.unshift(key);
+        localStorage.setItem('search_list',JSON.stringify(arr));
         render();
+        $('.search_input').val('');
+
     })
 
 
