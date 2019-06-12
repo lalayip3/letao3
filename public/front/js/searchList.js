bb84cb9c6e5254acb294fb89780e3fad3b78022e
yiphoipan <yiphoipan@yiphoipandeMacBook-Pro.local>
2019-06-12 14:04:43 +0800

    搜索列表功能

--- public/front/js/searchList.js
+++ public/front/js/searchList.js
@@ -8,26 +8,40 @@ $(function () {
     render()
 
     function render(){
-        $.ajax({
-            type:'get',
-            data:{
-                proName:$('.search_input').val(),
-                page:1,
-                pageSize:100
-            },
-            dataType:'json',
-            success:function (info) {
-                var htmlStr=template('productTpl',info);
-                $('.lt_product').html(htmlStr);
-            }
-        })
+        $('.lt_product').html(' <div class="loading"></div>');
+        var params={};
+        params.proName=$('.search_input').val();
+        params.page=1;
+        params.pageSize=100;
+        params.price = 2;
+
+        var current= $('.lt_sort a.current');
+        if (current.length>0){
+            var sortName=current.data('type');
+            var sortValue=current.find('i').hasClass('fa-angle-down')?2:1;
+            params[sortName]=sortValue;
+        }
+
+        setTimeout(function () {
+            $.ajax({
+                type:'get',
+                data:params,
+                dataType:'json',
+                success:function (info) {
+                    var htmlStr=template('productTpl',info);
+                    $('.lt_product').html(htmlStr);
+                }
+            })
+        },500)
+
+
 
     }
 
     $('.search_btn').click(function () {
         var key=$('.search_input').val();
         if (key.trim()===''){
-            alert('请输入关键字');
+           mui.toast('请输入搜索关键字');
             return;
         }
 
@@ -45,6 +59,18 @@ $(function () {
         arr.unshift(key);
         localStorage.setItem('search_list',JSON.stringify(arr));
     });
+
+    $('.lt_sort a[data-type]').click(function () {
+        if ($(this).hasClass('current')) {
+            $(this).find('i').toggleClass('fa-angle-up').toggleClass('fa-angle-down');
+        }
+        else{
+            $(this).addClass('current').siblings().removeClass('current');
+        }
+        render();
+    })
+
+
 
 
 })
