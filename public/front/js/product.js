2a4621763c98aa0f6b5f34c49689a26deaec3cb0
yiphoipan <yiphoipan@yiphoipandeMacBook-Pro.local>
2019-06-13 13:42:46 +0800

    商品详情页

---
+++ public/front/js/product.js
@@ -0,0 +1,57 @@
+$(function () {
+    var productId=getSearch('productId');
+
+    $.ajax({
+        type:'get',
+        url:'/product/queryProductDetail',
+        data:{
+            id:productId
+        },
+        dataType:'json',
+        success:function (info) {
+            var htmlStr=template('productTpl',info);
+            $('.lt_main .mui_scroll').html(htmlStr);
+            var gallery = mui('.mui-slider');
+            gallery.slider({
+                interval:5000//自动轮播周期，若为0则不自动播放，默认为0；
+            });
+        }
+    })
+
+    $('.lt_main').on('click','.lt_size span',function () {
+        $(this).addClass('current').siblings().removeClass('current');
+    })
+
+    $('#addCart').click(function () {
+        var size=$('.lt_size span.current').text();
+        var num =$('.mui-numbox-input').val();
+        if (!size){
+            mui.toast('请选择尺码');
+            return;
+        }
+
+        $.ajax({
+            type:'post',
+            url:'/cart/addCart',
+            data:{
+                productId:productId,
+                num:num,
+                size:size
+            },
+            dataType: 'json',
+            success:function (info) {
+                if (info.error ===400){
+                    location.href ='login.html?retUrl='+location.href;
+                }
+                if (info.success){
+                    mui.confirm("添加成功",'温馨提示',['去购物车','继续浏览'],function (e) {
+                        if (e.index ===0 ) {
+                            location.href = 'cart.html';
+                        }
+                    })
+                }
+            }
+        })
+
+    })
+})
